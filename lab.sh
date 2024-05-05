a1aa35cf5d5d9b454713a813f4c4002155b92d5c
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-05-05 23:35:35 +0700

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 student_data(){
-    #Fill Student Data
+    # Fill Student Data
     read -p "Enter Your Full Name: " name
     echo "$name" > /tmp/name
     read -p "Enter Your Email: " email 
@@ -16,7 +16,8 @@ send_data(){
     exercise_name=$(cat /tmp/exercise)
     mentor=$(cat /tmp/mentor)
     name=$(cat /tmp/name)
-    email=$(cat /tmp/rhnid)
+    email=$(cat /tmp/email)
+    rhnid=$(cat /tmp/rhnid)  # Corrected variable name
     score=$(cat /tmp/score)
     status=$(cat /tmp/status)
     id="https://script.google.com/macros/s/AKfycbzo4gj6N73xUcF0w0licSyWBqQp8YO2SWTCYhx7zINVa6Bw3k603kGHaNXEctXQufTv2Q/exec"
@@ -31,38 +32,37 @@ send_data(){
     -d "status=$status" &>/dev/null
 }
 
-
-if [ "$1" == "start" ];then
+if [ "$1" == "start" ]; then
     student_data
     exercise_name=$2
-    if [ "$2" == "epel" ];then
+    if [ "$2" == "epel" ]; then
         exercise_name="Extra Packages for Enterprise Linux (EPEL) for RHEL"
         
-    elif [ "$2" == "challenge-leapp" ];then
+    elif [ "$2" == "challenge-leapp" ]; then
         exercise_name="Challenge: Upgrade to Red Hat Enterprise Linux 9 in place with LEAPP"
         
-    elif [ "$2" == "run-container" ];then
+    elif [ "$2" == "run-container" ]; then
         exercise_name="Running containers on Red Hat Enterprise Linux"
 
-    elif [ "$2" == "build-container" ];then
+    elif [ "$2" == "build-container" ]; then
         exercise_name="Build container images with Red Hat Enterprise Linux container tools"
     
-    elif [ "$2" == "app-container" ];then
+    elif [ "$2" == "app-container" ]; then
         exercise_name="Build an App Into Container Image"
     
-    elif [ "$2" == "challenge1-container" ];then
+    elif [ "$2" == "challenge1-container" ]; then
         exercise_name="Challenge: Build Simple Apache Website Using Your Own Containerfile and Push The Container Image To quay.io Registry"
     
-    elif [ "$2" == "challenge2-container" ];then
+    elif [ "$2" == "challenge2-container" ]; then
         exercise_name="Challenge: Build NGINX Load Balancer with Container"
     
-    elif [ "$2" == "challenge-troubleshoot" ];then
+    elif [ "$2" == "challenge-troubleshoot" ]; then
         exercise_name="Challenge: Solve Issues of HTTPD & Executable App Permissions"
     
-    elif [ "$2" == "intro-git" ];then
+    elif [ "$2" == "intro-git" ]; then
         exercise_name="Intro to Git"
     
-    elif [ "$2" == "challenge-git" ];then
+    elif [ "$2" == "challenge-git" ]; then
         exercise_name="Challenge: Create systemd service from bash script that automatically synchronize to your github repo (auto pull & push) every 5 minutes"
     else
         echo "Exercise Content Not Found !!"
@@ -72,6 +72,6 @@ if [ "$1" == "start" ];then
     echo "You can start now !!"
     echo ""
     
-elif [ "$2" == "grade" ]
+elif [ "$1" == "grade" ]; then
     echo "GRADE SUCCESS"
 fi
