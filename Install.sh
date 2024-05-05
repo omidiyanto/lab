8bc71d82dac37fb21a1b361d62f03c8db344118e
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-05-05 23:38:44 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sudo curl -o /usr/bin/lab https://raw.githubusercontent.com/omidiyanto/lab/lab/lab.sh &>/dev/null
+sudo mkdir -p /tmp/sh &>/dev/null
+sudo chmod 777 /tmp/sh  &>/dev/null
+git clone -b lab https://github.com/omidiyanto/lab.git /tmp/sh &>/dev/null
+sudo cp /tmp/sh/lab.sh /usr/bin/lab &>/dev/null
 sudo chmod a+x /usr/bin/lab &>/dev/null
+sudo rm -rf /tmp/sh &>/dev/null
 echo -e "\e[1;42;97m LAB INSTALLED, YOU CAN LAB START NOW !! \e[0m"
 echo ""
 
