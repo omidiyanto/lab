ab7f91593fa5f95f06577ec8752500a3a5abe70a
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-05-05 23:25:40 +0700

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo curl -o /usr/bin/lab https://raw.githubusercontent........... &>/dev/null
+sudo chmod a+x /usr/bin/lab &>/dev/null
+echo -e "\e[1;42;97m LAB INSTALLED, YOU CAN LAB START NOW !! \e[0m"
+echo ""
+
+
+
