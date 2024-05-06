792ff8cec57fbffde34b3eebceb96a1aaf5db626
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-05-07 04:03:31 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-sudo mkdir -p /tmp/sh &>/dev/null
-sudo chmod 777 /tmp/sh  &>/dev/null
-git clone -b lab https://github.com/omidiyanto/lab.git /tmp/sh &>/dev/null
-sudo cp /tmp/sh/lab.sh /usr/bin/lab &>/dev/null
-sudo chmod a+x /usr/bin/lab &>/dev/null
-sudo rm -rf /tmp/sh &>/dev/null
-echo -e "\e[1;42;97m LAB INSTALLED, YOU CAN LAB START NOW !! \e[0m"
-echo ""
+# Encrypted 
+# By Infinite Learning
 
-
-
+z="
+";Zz='com/';lz='/sh/';DBz=' INS';Gz='/dev';Rz='git ';qz='lab ';JBz='STAR';Tz='e -b';HBz='CAN ';jz=' cp ';GBz='YOU ';OBz=' ""';pz='bin/';Qz='ull';tz=' rm ';xz=' -e ';mz='lab.';CBz=' LAB';iz='ll';EBz='TALL';kz='/tmp';ABz='1;42';Az='sudo';dz='ab.g';MBz=' \e[';Lz='77 /';rz='od a';bz='iyan';Oz='&>/d';vz='/sh ';Hz='/nul';BBz=';97m';gz='>/de';Sz='clon';Cz='ir -';Nz='sh  ';Yz='hub.';sz='+x /';hz='v/nu';oz='usr/';Vz=' htt';fz='sh &';cz='to/l';FBz='ED, ';Dz='p /t';wz='echo';LBz='W !!';Ez='mp/s';Bz=' mkd';nz='sh /';Uz=' lab';ez='it /';Xz='/git';az='omid';Iz='l';IBz='LAB ';Kz='od 7';Wz='ps:/';Jz=' chm';NBz='0m"';uz='-rf ';Pz='ev/n';Fz='h &>';Mz='tmp/';KBz='T NO';yz='"\e[';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$z$Az$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$z$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$Mz$fz$gz$hz$iz$z$Az$jz$kz$lz$mz$nz$oz$pz$qz$Oz$Pz$Qz$z$Az$Jz$rz$sz$oz$pz$qz$Oz$Pz$Qz$z$Az$tz$uz$kz$vz$Oz$Pz$Qz$z$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$z$wz$OBz"
