3b8cf7e227a4dc974d29029ab3c7e3e076e43f40
O. Midiyanto <90853331+omidiyanto@users.noreply.github.com>
2024-05-07 03:58:09 +0700

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -330,7 +330,7 @@ grade_challenge2-container() {
     
     echo -ne "Validate Load-Balancer is Working ....."
     for i in {1..15};do curl http://localhost:8080; sleep 1s; done &>/dev/null
-    podman exec -it my-load-balancer cat /var/log/nginx/demo-access.log | jq -r '.upstream_addr' | tr -s ', ' '\n' | sort | uniq | wc -l | grep 4 &> /dev/null
+    podman exec -it my-load-balancer cat /var/log/nginx/demo-access.log | jq -r '.upstream_addr' | tr -s ', ' '\n' | sort | uniq | wc -l | grep 3 &> /dev/null
     if [ $? -eq 0 ];then
         pass
         score=$(( score + 50 ))
